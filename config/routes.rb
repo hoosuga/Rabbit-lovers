c1e808f33174c6f239e1eb3fd6fb7f094346e29b
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-07-05 05:07:29 +0000

    [update] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, skip: [:registrations, :passwprds], controllers: {
+    sessions: "admins/sessions"
+  }
+  
+  devise_for :users, skip: [:passwords], controllers:{
+    registrations: "users/registrations",
+    sessions: "users/sessions"
+  } 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
