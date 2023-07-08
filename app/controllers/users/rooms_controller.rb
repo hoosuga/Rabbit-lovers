07d98d48ae02f01483c15b8d67b84bc116f3ae2d
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-07-08 09:29:43 +0000

    [add] 新規投稿機能

--- app/controllers/users/rooms_controller.rb
+++ app/controllers/users/rooms_controller.rb
@@ -1,8 +1,14 @@
 class Users::RoomsController < ApplicationController
   def index
+    @room = Room.new
+    
+    @rooms = Room.page(params[:page]).per(10)
   end
   
   def create
+    @room = Room.new(room_params)
+    @room.save
+    redirect_to room_path(@room.id)
   end
 
   def show
@@ -16,4 +22,11 @@ class Users::RoomsController < ApplicationController
   
   def destroy
   end
+  
+  private
+  
+  def room_params
+    params.require(:room).permit(:title, :body, :status, :user_id)
+  end
+  
 end
