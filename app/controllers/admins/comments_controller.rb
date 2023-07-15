d94fa04a360bf50de6446276300d8b34792f70df
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-07-15 09:23:36 +0000

    [Add] キーワード検索

--- app/controllers/admins/comments_controller.rb
+++ app/controllers/admins/comments_controller.rb
@@ -1,5 +1,11 @@
 class Admins::CommentsController < ApplicationController
   def index
+    if params[:search].present?
+      @comments = Comment.search(params)
+    else
+      @comments = Comment.all
+    end
+    @comments = @comments.page(params[:page]).per(10)
   end
   
   def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :room
+  
+  def self.search(params)
+    comments =  Comment.where('comments.body LIKE ?', "%#{params[:search]}%")
+    return comments
+  end
+  
 end
