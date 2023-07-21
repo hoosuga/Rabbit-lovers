b866046b5868b4cd6c6b62f58b6f64398d2403b0
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-07-21 10:39:07 +0000

    [Add] いいね機能

--- app/controllers/users/likes_controller.rb
+++ app/controllers/users/likes_controller.rb
@@ -3,8 +3,16 @@ class Users::LikesController < ApplicationController
   end
   
   def create
+    room = Room.find(params[:room_id])
+    like = current_user.likes.new(room_id: room.id)
+    like.save
+    redirect_to room_path(id: room.id)
   end
   
   def destroy
+    room = Room.find(params[:room_id])
+    like =current_user.likes.find_by(room_id: room.id)
+    like.destroy
+    redirect_to room_path(id: room.id)
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,7 @@ class Room < ApplicationRecord
   has_many :categories, through: :category_rooms
   belongs_to :user
   has_many :comments
-  has_many :likes
+  has_many :likes, dependent: :destroy
   
   
   enum status: { open: 0, close: 2 }
@@ -11,6 +11,10 @@ class Room < ApplicationRecord
   def self.search(params)
      Room.joins(:user, :categories).where('rooms.title LIKE ? OR rooms.body LIKE ? OR users.name LIKE ? OR categories.name LIKE ?',
                                                     "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").distinct
+  end
+  
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
   end
   
 end
