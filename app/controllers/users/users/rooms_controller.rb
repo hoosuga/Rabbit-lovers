2fb987321ae718dab4fa5c3f95bb08e911e67e9e
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-07-22 06:10:52 +0000

    [Update] カテゴリ検索機能

--- app/controllers/users/users/rooms_controller.rb
+++ app/controllers/users/users/rooms_controller.rb
@@ -7,38 +7,24 @@ class Users::Users::RoomsController < ApplicationController
 
     if @user == current_user
       if params[:search].present?
-        @rooms = @user.rooms.search(params).page(params[:page]).per(10)
+        @rooms = @user.rooms.search(params)
       elsif @category_ids.present?
         room_ids = CategoryRoom.where(category_id: @category_ids).pluck(:room_id).uniq
-        @rooms = @user.room.where(room_id: @room_ids).page(params[:page]).per(10)
+        @rooms = @user.rooms.where(id: room_ids)
       else
-        @rooms = @user.rooms.page(params[:page]).per(10)
+        @rooms = @user.rooms
       end
     else
       if params[:search].present?
         @rooms = @user.rooms.where(status: 0).search(params)
       elsif @category_ids.present?
         room_ids = CategoryRoom.where(category_id: @category_ids).pluck(:room_id).uniq
-        @rooms = @user.room.where(room_id: @room_ids).where(status: 0).page(params[:page]).per(10)
+        @rooms = @user.rooms.where(id: room_ids).where(status: 0)
       else
-        @rooms = @user.rooms.where(status: 0).page(params[:page]).per(10)
+        @rooms = @user.rooms.where(status: 0)
       end
     end
-    #@rooms = @rooms.page(params[:page]).per(10)
-    
-    #if @user == current_user
-      #if params[:search].present?
-       # @rooms = @user.rooms.search(params).page(params[:page]).per(10)
-      #else
-       # @rooms = @user.rooms.page(params[:page]).per(10)
-      #end
-    #else
-      #if params[:search].present?
-        #@rooms = @user.rooms.where(status: 0).search(params)
-      #else
-        #@rooms = @user.rooms.where(status: 0)
-      #end
-    #end
+    @rooms = @rooms.page(params[:page]).per(10)
     
   end
   
