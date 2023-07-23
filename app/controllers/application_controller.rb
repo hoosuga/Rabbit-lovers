f0f2475ccd773919c9987beca01351a7926f1f06
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-07-23 09:24:23 +0000

    [Add] アクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top, :about]
+  before_action :authenticate_admin!, if: :admins_url 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -25,4 +26,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+  
+  def admins_url
+    request.fullpath.include?("/admins")
+  end
+  
 end
