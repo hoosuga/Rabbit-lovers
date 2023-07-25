bcb164836c5d94c30e5a956baf59fa485d0286b1
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-07-25 11:09:19 +0000

    [Update] バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable #:validatable
          
   has_one_attached :profile_image
          
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   
   validates :name, presence: true
   validates :email, presence: true
+   validates :encrypted_password, presence: true
   
   def self.search(params)
     User.where('users.name LIKE ? OR users.introduction LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%")
