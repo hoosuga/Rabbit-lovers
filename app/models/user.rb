61b0ca8a0853333aec6bf1a9f8c1b9bbecfe4b60
hoosuga <130055556+hoosuga@users.noreply.github.com>
2023-08-10 08:57:48 +0000

    [Update] レイアウト調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [80, 80]).processed
+    profile_image.variant( resize: "80x80^", gravity: "center", crop: "80x80+0+0" ).processed
   end
   
   def self.guest
